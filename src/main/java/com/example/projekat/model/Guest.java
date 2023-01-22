f545bb01af262e70e08d579aed4cc37921c65981
Andrej <apecirep1@etf.unsa.ba>
2023-01-23 00:57:46 +0100

    added constructor to class Guest

--- src/main/java/com/example/projekat/model/Guest.java
+++ src/main/java/com/example/projekat/model/Guest.java
@@ -6,4 +6,16 @@ public class Guest {
     private String city;
     private String country;
     private String numberOfID;
+
+    public Guest(){
+
+    }
+
+    public Guest(String name, String lastName, String city, String country, String numberOfID) {
+        this.name = name;
+        this.lastName = lastName;
+        this.city = city;
+        this.country = country;
+        this.numberOfID = numberOfID;
+    }
 }
