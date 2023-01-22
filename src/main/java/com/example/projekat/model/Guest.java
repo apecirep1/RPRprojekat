8a2bcec66454267c06d3e80612caf17c4ee2f92c
Andrej <apecirep1@etf.unsa.ba>
2023-01-23 00:56:33 +0100

    added atributes to class Guest

--- src/main/java/com/example/projekat/model/Guest.java
+++ src/main/java/com/example/projekat/model/Guest.java
@@ -1,4 +1,9 @@
 package com.example.projekat.model;
 
 public class Guest {
+    private String name;
+    private String lastName;
+    private String city;
+    private String country;
+    private String numberOfID;
 }
