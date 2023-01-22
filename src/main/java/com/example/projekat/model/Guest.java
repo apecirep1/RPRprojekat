7a8106da8ea93bbf8e61845d35d6a7e31df8be14
Andrej <apecirep1@etf.unsa.ba>
2023-01-23 00:58:41 +0100

    added getter and setter to class Guest

--- src/main/java/com/example/projekat/model/Guest.java
+++ src/main/java/com/example/projekat/model/Guest.java
@@ -18,4 +18,44 @@ public class Guest {
         this.country = country;
         this.numberOfID = numberOfID;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getNumberOfID() {
+        return numberOfID;
+    }
+
+    public void setNumberOfID(String numberOfID) {
+        this.numberOfID = numberOfID;
+    }
 }
