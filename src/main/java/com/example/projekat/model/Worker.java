776f28e074fed77444800521b622ed96ab02de9b
Andrej <apecirep1@etf.unsa.ba>
2023-01-23 00:49:36 +0100

    created constructor for class Worker

--- src/main/java/com/example/projekat/model/Worker.java
+++ src/main/java/com/example/projekat/model/Worker.java
@@ -4,4 +4,14 @@ public class Worker {
     private String name;
     private String lastName;
     private String typeOfWork;
+
+    public Worker(){
+
+    }
+
+    public Worker(String name, String lastName, String typeOfWork) {
+        this.name = name;
+        this.lastName = lastName;
+        this.typeOfWork = typeOfWork;
+    }
 }
