f57d6658f7334ba1999bcafd1e0a33e44c28e8dc
Andrej <apecirep1@etf.unsa.ba>
2023-02-05 18:22:43 +0100

    added method getGuestFromResultSet

--- src/main/java/com/example/projekat/model/HotelDAO.java
+++ src/main/java/com/example/projekat/model/HotelDAO.java
@@ -66,4 +66,9 @@ public class HotelDAO {
         }
     }
 
+    private Guest getGuestFromResultSet(ResultSet rs) throws SQLException {
+        Guest guest = new Guest(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5));
+        return guest;
+    }
+
 }
