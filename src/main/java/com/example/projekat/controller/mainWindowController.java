91b573e315b963ef51e01ae3ad97c50f0e96521b
Andrej <apecirep1@etf.unsa.ba>
2023-02-06 12:38:26 +0100

    added makeReservation.fxml

--- src/main/java/com/example/projekat/controller/mainWindowController.java
+++ src/main/java/com/example/projekat/controller/mainWindowController.java
@@ -1,4 +1,19 @@
 package com.example.projekat.controller;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+
+
 public class mainWindowController {
+    public Button makeReservation;
+    public Button deleteReservation;
+    public Button changeReservation;
+    public Button exit;
+    public ChoiceBox moreOptions;
+    public TextField fieldSearch;
+    public Button search;
+
+
+
 }
