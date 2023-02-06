f18877d605ec3a7bcad07beaf7b2bf77281a11b3
Andrej <apecirep1@etf.unsa.ba>
2023-02-06 16:50:38 +0100

    added method actionMakeReservation

--- src/main/java/com/example/projekat/controller/MainWindowController.java
+++ src/main/java/com/example/projekat/controller/MainWindowController.java
@@ -1,8 +1,17 @@
 package com.example.projekat.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 
 public class MainWindowController {
@@ -15,5 +24,23 @@ public class MainWindowController {
     public Button search;
 
     public MainWindowController() {
+    }
+
+    public void actionMakeReservation(ActionEvent actionEvent) {
+        Stage stage = new Stage();
+        Parent root = null;
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/resources/makeReservation.fxml"));
+            MakeReservationController makeReservationController = new MakeReservationController();
+            loader.setController(makeReservationController);
+            root = loader.load();
+            stage.setTitle("Make Reservation");
+            stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+            stage.setResizable(false);
+            stage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
